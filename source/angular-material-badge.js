1556dbe0f79269e772c01068662e600386d8624e
Juan Manuel <jmouriz@gmail.com>
2017-09-03 16:30:31 -0300

    Add md-badge-color and md-badge-fill attributes

--- source/angular-material-badge.js
+++ source/angular-material-badge.js
@@ -1,6 +1,6 @@
-(function(window, angular, undefined) {
+(function(window, angular, document) {
    'use strict';
-   
+
    var module = angular.module('ngMdBadge', ['ngMaterial']);
 
    module.directive('mdBadge', ['$mdTheming', function($mdTheming) {
@@ -23,7 +23,27 @@
          link: function(scope, element, attributes) {
             $mdTheming(element);
             element.addClass('md-badge');
+				function apply(color, level) {
+               if (color) {
+                  color = color.replace(' ', '');
+                  window.rainbow = window.rainbow || {};
+                  if (!window.rainbow[color]) {
+                     var id = 'md-badge-' + Math.floor((Math.random() * 500) + 1);
+                     window.rainbow[color] = id;
+                     var style = document.createElement('style');
+                     style.type = 'text/css';
+                     if (level) {
+                        level += '-';
+                     }
+                     style.innerText = ['.', id, ':after', '{', level, 'color:', color, '!important', '}'].join('');
+                     document.head.insertBefore(style, document.head.firstChild);
+                  }
+               	element.addClass(window.rainbow[color]);
+               }
+				}
+            apply(attributes.mdBadgeFill, 'background');
+            apply(attributes.mdBadgeColor);
          },
       };
    }]);
-})(window, window.angular, undefined);
+})(window, window.angular, document);
